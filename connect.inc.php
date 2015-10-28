929026583db2da69088330f9cad21dcdf1a249c7
reddygk1 <reddygk1@student.op.ac.nz>
2015-10-28 16:35:50 +1300

    Added pages
    Need to Finalise

---
+++ connect.inc.php
@@ -0,0 +1,13 @@
+<?php
+	$host ="localhost";
+	$userMS="reddygk1";
+	$passwordMS="9003111";
+	$database = "reddygk1_IN612";
+	
+	//Establishing Connection.
+	
+	$connection = mysqli_connect($host,$userMS,$passwordMS, $database) or die("Couldn't connect");
+	
+
+?>	
+	
