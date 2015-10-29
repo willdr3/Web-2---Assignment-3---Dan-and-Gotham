c37d43c1a5e554d0d12a52c53519291963448cb8
willdr3 <willdr3@student.op.ac.nz>
2015-10-30 11:11:11 +1300

    Setting up login
    Changed to Dan's connect.inc page, created suitable login page

--- connect.inc.php
+++ connect.inc.php
@@ -1,13 +1,8 @@
 <?php
-	$host ="localhost";
-	$userMS="reddygk1";
-	$passwordMS="9003111";
-	$database = "reddygk1_IN612";
-	
-	//Establishing Connection.
-	
-	$connection = mysqli_connect($host,$userMS,$passwordMS, $database) or die("Couldn't connect");
-	
+$host = "localhost";
+$userMS = "willdr3";
+$passwordMS = "1000014417";
+$database = "willdr3_IN612";
 
-?>	
-	
+$connection = mysqli_connect($host, $userMS, $passwordMS, $database) or die ("Couldn't connect");
+?>
